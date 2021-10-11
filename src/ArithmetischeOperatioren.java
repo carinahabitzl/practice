7f5b0fe1cad202310b515d42485b9e3a1b3a1dd2
carinahabitzl <carina.habitzl@edu.campus02.at>
2021-10-11 10:30:03 +0200

    einfach Berechnungen durchgeführt

---
+++ src/ArithmetischeOperatioren.java
@@ -0,0 +1,12 @@
+public class ArithmetischeOperatioren {
+    public static void main(String[] args) {
+        int zahl = 9;
+        int zahl2 = 4;
+
+        System.out.println("a + b = " + zahl + zahl2  );
+        System.out.println("a - b = " + (zahl - zahl2));
+        System.out.println("a * b =" + (zahl*zahl2));
+        System.out.println("a / b = " + (zahl/zahl2));
+        System.out.println("a % b = " + (zahl%zahl2));
+}
+}
